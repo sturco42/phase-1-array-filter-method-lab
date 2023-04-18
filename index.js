ee18fe8f00092bfc4c56b3767e1d8ab71a743d27
Sam <samturco42@gmail.com>
2023-04-17 23:16:24 -0700

    actually done

--- index.js
+++ index.js
@@ -9,24 +9,3 @@ function fuzzyMatch(driverNames, string) {
 function matchName(driverNames, string) {
     return driverNames.filter(property => property.name === string)
 }
-
-// const string => string.charAt(0)
-// driverNames.filter.startsWith(string)
-// return fuzzyMatch
-
-// function findMatching(source, sought) {
-//     return source.filter(
-//       (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
-//     );
-//   }
-  
-//   function fuzzyMatch(source, testString) {
-//     return source.filter(
-//       (possibleMatch) =>
-//         possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
-//     );
-//   }
-  
-//   function matchName(source, soughtName) {
-//     return source.filter((record) => record.name === soughtName);
-//   }
